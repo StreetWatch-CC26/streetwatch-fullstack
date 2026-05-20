1a530e47a7be4edba328c5b0e7a1ccb2267bb6ea
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-05-20 18:24:03 +0700

    feat: add has complete onboarding cookies checker

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Layout({
   children,
@@ -8,9 +9,17 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   const cookieStore = await cookies();
-  const token = cookieStore.get("session-token")?.value;
+  const token = cookieStore.get("authjs.session-token")?.value;
 
   const isLoggedIn = !!token;
+
+  const completedOnboarding =
+    cookieStore.get("has-completed-onboarding")?.value === "true";
+
+  // Redirect jika user login tapi belum onboarding
+  if (!completedOnboarding) {
+    redirect("/welcome");
+  }
 
   return (
     <div>
