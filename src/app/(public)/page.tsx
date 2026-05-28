b3a5f73d3f1fcdffc44e4585208e335ebada6c60
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-05-28 12:46:20 +0700

    fix: add isLoggedIn var that get sessuin token cookies

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,14 +2,20 @@ import { CTASection } from "@/components/landing/CtaSection";
 import { FAQSection } from "@/components/landing/FaqSection";
 import { FeaturesSection } from "@/components/landing/FeaturesSection";
 import { HeroSection } from "@/components/landing/HeroSection";
-import { HowItWorksSection } from "@/components/landing/HowItWorksSection ";
+import { HowItWorksSection } from "@/components/landing/HowItWorksSection";
 import { ImpactSection } from "@/components/landing/ImpactSection";
 
+import { cookies } from "next/headers";
+
 export default async function LandingPage() {
+  const cookieStore = await cookies();
+    const token = cookieStore.get("authjs.session-token")?.value;
+  
+    const isLoggedIn = !!token;
   return (
     <main className="flex flex-col items-center justify-center">
       <div className=" min-h-dvh w-full">
-        <HeroSection />
+        <HeroSection isLoggedIn={isLoggedIn}/>
         <FeaturesSection />
         <HowItWorksSection />
         <ImpactSection />
