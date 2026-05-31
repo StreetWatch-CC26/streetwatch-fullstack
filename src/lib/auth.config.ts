46a108aa15f73bf686da3e99612cb6aa168bc5b1
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-05-31 08:28:41 +0700

    feat: add trust host

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -4,6 +4,7 @@ import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
   session: { strategy: "jwt" },
+  trustHost: true,
   pages: {
     signIn: "/login",
     error: "/login",
