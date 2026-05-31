996db6fe7b3493e32517d4c12fcea285fd149937
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-06-01 03:10:34 +0700

    style: make page subtitle hiding with screen reader in mobile view

--- src/app/dashboard/reports/new/page.tsx
+++ src/app/dashboard/reports/new/page.tsx
@@ -3,18 +3,18 @@ import { AddReportForm } from "@/components/add-report/AddReportForm";
 export default function AddReportPage() {
   return (
     <div className="max-w-4xl mx-auto px-4 py-6 md:py-8">
-      <div className="flex flex-col items-center sm:items-start mb-6 md:mb-8">
-        <h1 className="text-3xl sm:text-4xl font-bold text-foreground mb-2">
+      <div className="flex flex-col items-center sm:items-start mb-5 sm:mb-7">
+        <h1 className="text-2xl sm:text-4xl font-bold text-foreground mb-2">
           Laporkan Kerusakan Jalan
         </h1>
 
-        <p className="text-muted-foreground text-sm md:text-base text-center sm:text-left ">
+        <p className="sr-only sm:not-sr-only text-muted-foreground text-sm md:text-base text-center sm:text-left ">
           Bantu kami memperbaiki infrastruktur dengan melaporkan kerusakan jalan
           di sekitar Anda
         </p>
       </div>
 
-      <div className="w-full px-5 sm:px-0">
+      <div className="w-full">
         <AddReportForm />
       </div>
     </div>
